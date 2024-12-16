3e736d165d848c529890eb63ebbcba1b80cd0ffa
elenakarpilenko <elenakarpilenko787@gmail.com>
2024-12-16 17:46:46 +0600

    First

---
+++ HW13Tests/HW13Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HW13Tests.swift
+//  HW13Tests
+//
+//  Created by Елена Карпиленко on 10/12/24.
+//
+
+import Testing
+@testable import HW13
+
+struct HW13Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
